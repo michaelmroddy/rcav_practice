32e4f22653fe8b83bbafb578d71a077aa9383888
Michael Roddy <michaelmroddy@gmail.com>
2016-11-08 07:01:29 -0600

    Working
    Kinda

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -2,4 +2,20 @@ class CalculationsController < ApplicationController
   def instructions
     render("instructions.html.erb")
   end
+
+  def squareroot
+    @num= params[:num].to_f
+    @sqrnum = Math.sqrt(@num).round(2)
+  end
+  def randomize
+    @low = params[:low].to_i
+    @high = params[:high].to_i
+    @rand = @low+Random.rand(@high-@low)
+  end
+  def calcpay
+    @irate = (params[:irate].to_i)/100.0
+    @years = params[:years].to_i
+    @amount = params[:amount].to_i
+    @payment = ((@irate/12/100 * @amount)/(1-((1+@irate/12/100)**(-@years*12)))).round(2)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   get("/", { :controller => "calculations", :action => "instructions" })
+  get("/square_root/:num", { :controller => "calculations", :action => "squareroot" })
+  get("/random/:low/:high", { :controller => "calculations", :action => "randomize" })
+  get("/payment/:irate/:years/:amount", { :controller => "calculations", :action => "calcpay" })
 end
